b6ae67521b3f29732c4bccb245b09f4a78d24d2d
Maria McKinley <mariak@mariakathryn.net>
2014-01-02 10:44:39 -0800

    made a routine in check_positions to check size of square in visual angle

--- check_positions.py
+++ check_positions.py
@@ -50,6 +50,8 @@ class World(DirectObject):
         self.accept('space', self.next)
         self.accept('a', self.all)
         self.accept('d', self.degree_positions)
+        self.accept('s', self.change_square_size)
+
         self.mode = 0
         self.pos = []
         #self.root = self.base.render.attachNewNode("Root")
@@ -94,6 +96,37 @@ class World(DirectObject):
             size = max - min
             #print size[0], size[2]
 
+    def change_square_size(self):
+        if self.mode == 0:
+            circle_node = self.make_circle()
+            pos = Positions(self.config).get_position(self.depth)
+            self.mode = 1
+        res = [1024, 768]
+        # Screen size
+        screen = [1337, 991]
+        v_dist = 1219
+        b = 0
+        scale = 1
+        for i, j in enumerate(pos):
+            #b += 0.04  # covers all of the values if using 25 points
+            #b += 0.08
+            b += 0.03
+            scale += 0.1
+            #print b
+            #print i
+            #print j
+            square = self.make_square(scale)
+            square.setPos(Point3(j))
+            #print square.getPos()
+            #print square.getTightBounds()
+            min, max = square.getTightBounds()
+            size = max - min
+            print size[0], size[2]
+            deg_per_pixel = visual_angle(screen, res, v_dist)
+            print deg_per_pixel
+            print 'size in degrees, x', size[0] * deg_per_pixel[0]
+            print 'size in degrees, y', size[2] * deg_per_pixel[1]
+
     def degree_positions(self):
         # set center, than 4 squares in cardinal directions at interval of 5 degree angles
         if self.mode == 0:
@@ -112,12 +145,15 @@ class World(DirectObject):
             except StopIteration:
                 print 'done'
 
-    def make_square(self):
+    def make_square(self, scale = []):
         square = self.base.loader.loadModel("models/plane")
         square.reparentTo( render )
         #square.ls()
         #square.setScale(0.05)
-        square.setScale(8.5)
+        if scale:
+            square.setScale(scale)
+        else:
+            square.setScale(8.5)
         square.setDepthTest(False)
         square.setTransparency(1)
         square.setTexture(self.base.loader.loadTexture("textures/calibration_square.png"), 1)
@@ -126,11 +162,11 @@ class World(DirectObject):
         # yellow
         #square.setColor(175 / 255, 175 / 255, 130 / 255, 1.0)
         square.setColor(0.9, 0.9, 0.6, 1.0)
-        print square.getX()
-        print square.getY()
+        #print square.getX()
+        #print square.getY()
         min, max = square.getTightBounds()
         size = max - min
-        print size[0], size[2]
+        #print size[0], size[2]
         return square
 
     def make_circle(self, angle_deg = 360):
